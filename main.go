beacf19883d5a407367fa76e748bf121324d12d8
jaavier <javier.guajardoj@gmail.com>
2024-09-03 19:38:21 +0200

    Added handler for interacting with LLM in the most basic way

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"app/gemini"
+	"fmt"
 	"log"
 )
 
@@ -8,4 +10,12 @@ func main() {
 	if err := loadEnv("./.env"); err != nil {
 		log.Fatal(err)
 	}
+	var prompt string
+	fmt.Print("Prompt: ")
+	fmt.Scanln(&prompt)
+	res, err := gemini.Request(prompt)
+	if err != nil {
+		log.Fatal("Error on request:", err)
+	}
+	fmt.Println("AI answer:", res)
 }
